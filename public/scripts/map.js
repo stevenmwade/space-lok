196e75696a7a02f6617475b7c0e0b3dcb8704c8d
Steven Wade <stevenmwade@gmail.com>
2015-02-26 11:01:32 -0700

    add pictures to facilities, company logos and tri description on index, small style changes on pages

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,10 +1,17 @@
 function initialize() {
 	var mapOptions = {
-		center: {lat: 0, lng: 0},
-		zoom: 12
+		center: { lat: 33.911578, lng: -118.301949},
+		zoom: 14
 	};
-	var map = new google.maps.Map(document.getElementById('contactMap'), mapOptions);
 
+	var map = new google.maps.Map(document.getElementById('contact-map'),
+						mapOptions);
+
+	var centerMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(33.911578, -118.301949),
+		map: map,
+		title: 'Space-Lok, Inc.'
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
